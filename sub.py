8b906bbb343adee2cfd91f33d7e04444bdcdefd8
tushar93git <132362201+tushar93git@users.noreply.github.com>
2023-05-05 18:03:13 +0530

    git commit -m "sub"

---
+++ sub.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# subtract two numbers
+sub = num1 - num2
+
+# Display the sum
+print('The sub of {0} and {1} is {2}'.format(num1, num2, sub))
